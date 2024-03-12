54613806904a5e2dec40c3b3f61e4e7a866075ce
Èric Marès <ericmares13@gmail.com>
2024-03-12 14:31:12 +0100

    feature(logger): Persistencia del logger en base de datos

--- src/server/middlewares/loggingMiddleware.js
+++ src/server/middlewares/loggingMiddleware.js
@@ -1,9 +1,11 @@
 const loggingMiddleware = (db) =>
     (req, res, next) => {
         const ip = (req.headers['x-forwarded-for'] || req.connection.remoteAddress || '').split(',')[0].trim();
-        const headers = JSON.stringify(req.headers);
+        const header = JSON.stringify(req.headers);
         const originalUrl = req.originalUrl;
+        const action = req.url
         // Persist this info on DB
+        db.logging.create({ ip, header, originalUrl , action})
         next();
     }
 
